e3978035b4fc0223c4f4440a270c7e1607555d97
goodsmell <87801306+goodsmell@users.noreply.github.com>
2023-01-24 22:11:32 +0900

    Minigame5,7 수정
    미니게임 5번-  아기 눈 수정, socket 수정
    미니게임 7번 - grab하면 장난감 이름 뜨게 수정 (grab하면 거꾸로 보이는 문제점 수정 예정) , 빛 수정, layer 겹치는 문제 수정

---
+++ Unity/Assets/minigame_eunhyang/script/Select_object name/SelectObject_1.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class SelectObject_1 : MonoBehaviour
+{   
+    TMP_Text object_name;
+    
+    public void OnSelectEntered()
+    {   
+       
+        object_name = GameObject.Find("text_1").GetComponent<TextMeshPro>();
+        
+        object_name.text = $"{gameObject.name}";
+        
+    }
+
+    public void OnSelectExited()
+    {
+        object_name.text = " ";
+    
+    }
+    
+}
