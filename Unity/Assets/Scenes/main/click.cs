fac8264a4ccc64aa7af7e5686c7d00d3ef6a7df5
kimsohee72 <cowhappy721@naver.com>
2023-01-25 10:02:12 +0900

    은향이 아이 에셋 변경/main화면 추가/main에서 각각의 씬으로 이동하도록 설정/minigame4 빛 추가

---
+++ Unity/Assets/Scenes/main/click.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class click : MonoBehaviour
+{
+    public void SceneChange1()
+    {
+        SceneManager.LoadScene("Mini1");
+    }
+    public void SceneChange2()
+    {
+        SceneManager.LoadScene("Slice_game");
+    }
+    public void SceneChange3()
+    {
+        SceneManager.LoadScene("no.3");
+    }
+    public void SceneChange4()
+    {
+        SceneManager.LoadScene("minigame4");
+    }
+    public void SceneChange5()
+    {
+        SceneManager.LoadScene("Minigame_eunhyang5");
+    }
+    public void SceneChange6()
+    {
+        SceneManager.LoadScene("no.6");
+    }
+    public void SceneChange7()
+    {
+        SceneManager.LoadScene("Minigame_eunhyang7");
+    }
+}
