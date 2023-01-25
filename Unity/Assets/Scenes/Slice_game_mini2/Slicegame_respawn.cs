02d95be99268b5e7433f40455dc5daef6fc5957d
JeongEon8 <113885866+JeongEon8@users.noreply.github.com>
2023-01-26 00:39:45 +0900

    Slicegame(0126 00:39 Update)
    칼 수정

--- Unity/Assets/Scenes/Slice_game_mini2/Slicegame_respawn.cs
+++ Unity/Assets/Scenes/Slice_game_mini2/Slicegame_respawn.cs
@@ -4,16 +4,60 @@ using UnityEngine;
 
 public class Slicegame_respawn : MonoBehaviour
 {
-    public GameObject Slicegame_Knife;
+    public GameObject Slicegame_grab;
     Transform target;
 
-    void OnTriggerEnter(Collider col)
+    public void OnFirstHoverEntered()
     {
-        if (col.gameObject.tag == "Knife")
-		{
-            target = Slicegame_Knife.GetComponent<Transform>();
-            target.position = new Vector3(-3.6f, 1.3f, 0.7f);
-            target.rotation = Quaternion.Euler(0, 90, 0);
-		}
+
     }
+
+    public void OnLastHoverExited()
+    {
+
+    }
+
+    public void OnHoverEntered()
+    {
+
+    }
+
+    public void OnHoverExited()
+    {
+
+    }
+
+    public void OnFirstSelectEntered()
+    {
+
+    }
+
+    public void OnLastSelectExited()
+    {
+
+    }
+
+    public void OnSelectEntered()
+    {
+        Slicegame_grab.SetActive(false);
+    }
+
+    public void OnSelectExited(GameObject Slicegame_Knife)
+    {
+        target = Slicegame_Knife.GetComponent<Transform>();
+        target.position = new Vector3(-3.6f, 1.3f, 0.7f);
+        target.rotation = Quaternion.Euler(270, 90, 0);
+        Slicegame_grab.SetActive(true);
+    }
+
+    public void OnActivated()
+    {
+
+    }
+
+    public void OnDeactivated()
+    {
+
+    }
+
 }
