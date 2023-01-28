6c6116b5430f86d3c3198305db4178565ac91d37
pipi-shortstocking <jannie435@naver.com>
2023-01-28 22:33:59 +0900

    7번 list 수정

---
+++ Unity/Assets/minigame_eunhyang/script/toy/magnet.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class magnet : MonoBehaviour
+{
+    Rigidbody rigid;
+    public LayerMask worldLayer;
+    Ray ray;
+    int high;
+    TMP_Text list;
+    public GameObject text;
+
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody>();
+        if (text.activeSelf == true)
+            list = GameObject.Find("list_magnet").GetComponent<TextMeshPro>();
+    }
+
+    void Update()
+    {
+        ray = new Ray(rigid.position, Vector3.down);
+        high = LayerMask.NameToLayer("high");
+    }
+
+    void FixedUpdate()
+    {
+        Debug.DrawRay(rigid.position, Vector3.down * 0.1f, new Color(0, 1, 0));
+
+        if (Physics.Raycast(ray, 0.5f, 1 << high))
+        {
+            //Debug.Log("high");
+            list.text = "ÀÚ¼®";
+            list.color = new Color(0, 0, 0, 1);
+        }
+    }
+}
