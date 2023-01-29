4f82ac1a61095f2f5b9a36a64b62af4af7f722ff
aneunki <84065453+aneunki@users.noreply.github.com>
2023-01-29 14:40:06 +0900

    23.01.29 14:40 가이드라인 위치 변경, 시작화면 위치 변경
    가이드라인 위치 변경 및 상황 별 가이드라인 출력
    시작화면 위치 변경

--- Unity/Assets/MINI1_Script/startButtonClick.cs
+++ Unity/Assets/MINI1_Script/startButtonClick.cs
@@ -18,6 +18,9 @@ public class startButtonClick : MonoBehaviour
     GameObject panelGuide2;
     GameObject panelGuide3;
 
+    GameObject babyChair;
+    GameObject bib;
+
     // Start is called before the first frame update
     void Awake()
     {
@@ -34,6 +37,8 @@ public class startButtonClick : MonoBehaviour
         panelGuide1 = GameObject.Find("PanelGuide1");
         panelGuide2 = GameObject.Find("PanelGuide2");
         panelGuide3 = GameObject.Find("PanelGuide3");
+        babyChair = GameObject.Find("HighChair_01");
+        bib = GameObject.Find("Bib_01");
 
         baby.SetActive(false);
         rattle.SetActive(false);
@@ -45,6 +50,8 @@ public class startButtonClick : MonoBehaviour
         panelGuide1.SetActive(false);
         panelGuide2.SetActive(false);
         panelGuide3.SetActive(false);
+        babyChair.SetActive(false);
+        bib.SetActive(false);
 
     }
 
@@ -65,6 +72,8 @@ public class startButtonClick : MonoBehaviour
         panelStart.SetActive(false);
         panelGuide.SetActive(true);
         panelGuide1.SetActive(true);
+        babyChair.SetActive(true);
+        bib.SetActive(true);
 
     }
 }
